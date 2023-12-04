ac6f45b4f3acd557e53b4b822da74f9fd37c5085
Fahmi <fahmiardiyannto@yahoo.com>
2023-12-04 15:45:58 +0700

    Setup Project

---
+++ ios-githubUser/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ios-githubUser
+//
+//  Created by Muhammad Fahmi on 04/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ios-githubUser/ios_githubUserApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_githubUserApp.swift
+//  ios-githubUser
+//
+//  Created by Muhammad Fahmi on 04/12/23.
+//
+
+import SwiftUI
+
+@main
+struct ios_githubUserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
